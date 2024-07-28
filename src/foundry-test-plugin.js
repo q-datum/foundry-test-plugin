c60682de3b2a2d7d389844a01a114539a22d6808
q-datum <q.datum@gmail.com>
2024-07-28 02:03:37 +0200

    sets player do reactions to none

--- src/foundry-test-plugin.js
+++ src/foundry-test-plugin.js
@@ -1,7 +1,18 @@
-Hooks.on("init", function() {
-  console.log("Test Plugin | I'm alive");
+const PLUGIN_NAME = 'Test Plugin';
+const log = ( value ) => { console.log(`${PLUGIN_NAME} | ${value}`); }
+
+Hooks.on("init", function () {
+    console.log("Test Plugin | I'm alive");
 });
 
-Hooks.on("ready", function() {
-  console.log("Test Plugin | I'm ready..");
+Hooks.on("ready", function () {
+    log("I'm ready..");
+    console.log(game.modules.get("midi-qol"));
+
+    // let settings = game.settings.get("midi-qol");
+    const midiQOLSettings = game.settings.get("midi-qol", "ConfigSettings");
+    log(midiQOLSettings.doReactions);
+    midiQOLSettings.doReactions = 'none';
+    log(midiQOLSettings.doReactions);
+    console.log(game.settings);
 });
