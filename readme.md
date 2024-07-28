4359ecdd38898a3ebf1ed98d2374761244df8fe5
q-datum <q.datum@gmail.com>
2024-07-28 02:16:29 +0200

    added comments

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Test Plugin For Foundry VTT
+
+To install copy this folder to 
+> { User }\AppData\Local\FoundryVTT\Data\modules
--- src/foundry-test-plugin.js
+++ src/foundry-test-plugin.js
@@ -1,18 +1,23 @@
 const PLUGIN_NAME = 'Test Plugin';
+// prints output to the dev tools (press f12)
 const log = ( value ) => { console.log(`${PLUGIN_NAME} | ${value}`); }
 
+// triggers while the vtt initializes
 Hooks.on("init", function () {
     console.log("Test Plugin | I'm alive");
 });
 
+// triggers when the vtt is ready
 Hooks.on("ready", function () {
     log("I'm ready..");
+    // get manifest data of the midi-qol module
     console.log(game.modules.get("midi-qol"));
 
-    // let settings = game.settings.get("midi-qol");
+    // access settings of the midi-qol module
     const midiQOLSettings = game.settings.get("midi-qol", "ConfigSettings");
+
     log(midiQOLSettings.doReactions);
-    midiQOLSettings.doReactions = 'none';
+    midiQOLSettings.doReactions = 'none'; // disables auto reaction for players
     log(midiQOLSettings.doReactions);
-    console.log(game.settings);
+    console.log(midiQOLSettings);
 });
